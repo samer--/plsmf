9727c632a90bdd601286e82f5d47bf5f6924a524
Samer Abdallah <samer@holly.home>
2015-11-21 21:54:14 +0000

    Add new features based on new foreign library features
    Added new predicates smf_duration/3, smf_property/2 and smf_tempo/4.
    Also revised event getting predicates to be able to select track
    and time units as seconds or pulses.

--- prolog/plsmf.pl
+++ prolog/plsmf.pl
@@ -20,9 +20,14 @@
    ,  smf_write/2
 	,	smf_description/2
 	,	smf_duration/2
+	,	smf_duration/3
 	,	smf_events/2
+	,	smf_events/3
+	,	smf_events/4
 	,	smf_events_between/4
    ,  smf_add_events/2
+   ,  smf_property/2
+   ,  smf_tempo/3
 	,	is_smf/1	
 	]).
 	
@@ -40,9 +45,12 @@
 %  to an SMF blob atom which can be used to make further queries
 %  about the file.
 
+%% smf_duration( +Ref:smf_blob, +Timeline:oneof([metrical,physical]), -Dur:nonneg) is det.
 %% smf_duration( +Ref:smf_blob, -Dur:nonneg) is det.
 %
-%  Returns the duration of the MIDI file in seconds.
+%  Returns the duration of the MIDI file in seconds (Timeline=physical) or
+%  pulses (Timeline=metrica). smf_read/2 assumes physical timeline.
+smf_duration(Ref,Dur) :- smf_duration(Ref,physical,Dur).
 
 %% smf_description( +Ref:smf_blob, -Desc:atom) is det.
 %
@@ -62,12 +70,24 @@
 %               ; smf( nonneg, byte, byte, byte).
 %
 %  @see smf_events_between/4.
+smf_events(Ref,Events) :- smf_events(Ref,physical,Events).
+smf_events(Ref,Timeline,Events) :- smf_events(Ref,all,Timeline,Events).
+
+smf_events(Ref,all,Timeline,Events) :- 
+   timeline(Timeline),
+   smf_events_without_track(Ref,0,Timeline,-1,-1,Events).
+smf_events(Ref,track(T),Timeline,Events) :- 
+   timeline(Timeline),
+   smf_property(Ref,tracks(N)), between(1,N,T),
+   smf_events_without_track(Ref,T,Timeline,-1,-1,Events).
 
 %% smf_events_between( +Ref:smf_blob, +T1:nonneg, +T2:nonneg, -Events:list(smf_event)) is det.
 %
 %  Unifies Events with a list containing events in the MIDI file
 %  between the given times T1 and T2. See smf_events/2 for more
 %  information about the format of the events list.
+smf_events_between(Ref,T1,T2,Events) :-
+   smf_events_without_track(Ref,0,physical,T1,T2,Events).
 
 %% is_smf(+Ref) is semidet.
 %
@@ -75,6 +95,22 @@
 %  by smf_read/2.
 
 
+smf_property(Ref,Prop) :-
+   member(Key, [ppqn, fps, tracks, resolution]),
+   Prop =.. [Key,Val],
+   smf_info(Ref,Key,Val).
+
+smf_tempo(Ref,seconds(T),Prop) :- smf_tempo(Ref,physical,T,Tempo), tempo_property(Tempo,Prop).
+smf_tempo(Ref,pulses(T),Prop)  :- smf_tempo(Ref,metrical,T,Tempo), tempo_property(Tempo,Prop).
+
+tempo_property(smf_tempo(T,_,_,_,_,_,_),time(metrical,T)).
+tempo_property(smf_tempo(_,T,_,_,_,_,_),time(physical,T)).
+tempo_property(smf_tempo(_,_,N,_,_,_,_),crochet_duration(D)) :- D is N rdiv 1000000.
+tempo_property(smf_tempo(_,_,N,_,_,_,_),crochets_per_minute(D)) :- D is 60000000 rdiv N.
+tempo_property(smf_tempo(_,_,_,N,D,_,_),time_signature(N/D)).
+
+timeline(metrical).
+timeline(physical).
 /*
 	MIDI derived event types:
 
